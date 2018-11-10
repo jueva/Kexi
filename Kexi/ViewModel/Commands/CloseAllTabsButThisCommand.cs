542c94883bc2c07c9eb62e000b543a974577a983
jueva <jueva@users.noreply.github.com>
2018-11-10 23:22:34 +0100

    Close all tabs but this command

---
+++ Kexi/ViewModel/Commands/CloseAllTabsButThisCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Kexi.Common;
+using Kexi.Interfaces;
+
+namespace Kexi.ViewModel.Commands
+{
+    [Export]
+    [Export(typeof(IKexiCommand))]
+    public class CloseAllTabsButThisCommand : IKexiCommand
+    {
+        [ImportingConstructor]
+        public CloseAllTabsButThisCommand(Workspace workspace)
+        {
+            _workspace = workspace;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var otherListers = _workspace.Docking.Files.Where(f => f.Content != _workspace.ActiveLister).ToArray();
+            foreach (var lister in otherListers)
+                _workspace.Docking.Files.Remove(lister);
+        }
+
+        public event EventHandler  CanExecuteChanged;
+        private readonly Workspace _workspace;
+    }
+}
