e6374eb175f04e802d32013dbdd582ea41790546
jueva <jueva@users.noreply.github.com>
2018-11-13 21:20:32 +0100

    Prepare undo

---
+++ Kexi/ViewModel/Commands/UndoCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Kexi.Interfaces;
+
+namespace Kexi.ViewModel.Commands
+{
+    [Export]
+    [Export(typeof(IKexiCommand))]
+    public class UndoCommand : IKexiCommand
+    {
+        [ImportingConstructor]
+        public UndoCommand(Workspace workspace)
+        {
+            _workspace = workspace;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            _workspace.NotificationHost.AddInfo("Undo actually not supported. To  undo last fileaction open windows explorer and press ctrl+z");
+        }
+
+        public event EventHandler  CanExecuteChanged;
+        private readonly Workspace _workspace;
+    }
+}
